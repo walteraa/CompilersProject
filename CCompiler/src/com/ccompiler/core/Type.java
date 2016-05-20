32c719460429cd59d2b48533bf0ff7d9a0ced898
Walter Alves <walter.alves@ccc.ufcg.edu.br>
2016-05-20 02:25:10 -0300

    some fixes

--- CCompiler/src/com/ccompiler/core/Type.java
+++ CCompiler/src/com/ccompiler/core/Type.java
@@ -10,19 +10,21 @@ public class Type extends NamedEntity implements Parameter {
 	public Type(String name) {
 		super(name);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "{ Type " + getName() + " }";
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Type))
 			return false;
-		return getName().equals(((Type) obj).getName());
+		return getName().equals(((Type) obj).getName())
+				|| ((Type) obj).getName().equals("char") && this.getType().getName().equals("int")
+				|| ((Type) obj).getName().equals("int") && this.getType().getName().equals("char");
 	}
-	
+
 	public Type getType() {
 		return this;
 	}
